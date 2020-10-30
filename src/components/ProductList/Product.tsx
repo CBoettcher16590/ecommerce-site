9f072509dd7018c52b5700d69da4679c0df2868e
Colby <colby.boettcher@gmail.com>
2020-10-29 19:28:56 -0700

    CodeAlong update

--- src/components/ProductList/Product.tsx
+++ src/components/ProductList/Product.tsx
@@ -3,7 +3,7 @@ import { Card } from 'react-bootstrap';
 
 export default function Product( props:{} ){
 
-    return <Card>
+    return <Card className="card-columns">
         <Card.Img variant="top" src="holder.js/100px160" />
         <Card.Body>
         <Card.Title>Card title</Card.Title>
